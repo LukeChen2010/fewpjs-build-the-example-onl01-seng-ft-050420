d278e959a9e8d07ab15954a85e1afa3d770a6d49
Luke Chen <luke.chen.2010@gmail.com>
2020-07-26 17:26:26 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+for (let glyph of articleHearts) 
+{
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
